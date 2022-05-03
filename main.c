9beaf2741326c07b90d9a32586fa1db401e5237a
Marco <markcostah@gmail.com>
2022-05-03 19:44:44 +0100

    função do adversário

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main()
     winner = ' ';
 
     while(winner == ' '){
+        // vez do primeiro jogador
         int px = 0, py = 0; // guardar posicao da ultima jogada do jogador
         player_move(mini_board, &px,&py);
         check_win_miniBoard(mini_board);
@@ -29,6 +30,21 @@ int main()
             mini_board++;
         }
 
+        // vez do segundo jogador
+        int ox = 0, oy = 0;
+        opponent_move(mini_board,&ox,&oy);
+        check_win_miniBoard(mini_board);
+        winner = check_win();
+        if(winner != ' '){
+            // existe um vencedor
+            printf("Vencedor: %c", winner);
+            break;
+        }
+        mini_board = calculate_mini_board(&ox,&oy);
+        while(board_grid[mini_board].winner != ' '){
+            printf("Esta mini-board ja tem vencedor, por isso, foi escolhida outra.");
+            mini_board++;
+        }
     }
 
     return 0;
